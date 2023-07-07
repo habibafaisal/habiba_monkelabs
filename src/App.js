5cd102795637227af16797c79d41ebfe4be69c6b
habibafaisal <habibafaisal8@gmail.com>
2023-07-08 01:28:01 +0500

    Final Commits v0.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
-import ChatRoom from "./containers/ChatRoom";
 import ChatPage from "./containers/ChatPage";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<ChatPage />} />
-        {/* <Route path="/" element={<ChatRoom />} /> */}
       </Routes>
     </Router>
   );
